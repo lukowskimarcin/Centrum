e78e145cf7d92a610ab3f08b2f68ebfb4aee033c
Marcin <Marcin@pclukowski>
2016-08-29 16:40:19 +0200

    p

--- Centrum/Centrum/index.php
+++ Centrum/Centrum/index.php
@@ -4,6 +4,7 @@
 <title><?php wp_title(); ?> <?php bloginfo('name'); ?></title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="google-site-verification" content="RYKy-OxKhwkAVxtc2fvwZPWWaKU8j2heR6Xtt-87NLs" />
 
 <meta http-equiv="content-language" content="pl">
 <link hreflang="pl" href="http://centrumstatystyczne.pl/" rel="alternate" />
