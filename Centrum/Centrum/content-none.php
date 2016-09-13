a26607e5617c8d64e0b11dddc94297a9827753c1
Marcin <Marcin@DESKTOP-MDOJRNC>
2016-09-13 21:03:50 +0200

    dom

--- Centrum/Centrum/content-none.php
+++ Centrum/Centrum/content-none.php
@@ -5,6 +5,7 @@
 ?>
 
 <div   class="alert alert-warning search-result no-result">
+		<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
 		<h2><?php _e( 'Nothing Found', 'twentyfifteen' ); ?></h2>
 		<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'twentyfifteen' ); ?></p>
 	</div>
--- Centrum/Centrum/search.php
+++ Centrum/Centrum/search.php
@@ -8,26 +8,27 @@
 
 
 		<div class="alert alert-info search-result">
+			<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
 			<h2 class="page-title"><?php printf( 'Wynik wyszukiwania dla: <strong>%s</strong>', get_search_query() ); ?></h2>
 		</div>
 		 
 
-			<?php
-			// Start the loop.
-			while ( have_posts () ) :
-				the_post ();
-				?>
+		<?php
+		// Start the loop.
+		while ( have_posts () ) :
+			the_post ();
+			?>
 
-				<?php
-				/*
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part ( 'content', 'search' );
-				
-				// End the loop.
-			endwhile;
+			<?php
+			/*
+			 * Run the loop for the search to output the results.
+			 * If you want to overload this in a child theme then include a file
+			 * called content-search.php and that will be used instead.
+			 */
+			get_template_part ( 'content', 'search' );
+			
+			// End the loop.
+		endwhile;
 		 		
 
 		// If no content, include the "No posts found" template.
