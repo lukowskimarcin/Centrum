f97127a9ee0a532489dbcc3cea8cd5c82d12b3c5
Marcin <Marcin@DESKTOP-MDOJRNC>
2016-09-16 00:05:19 +0200

    tytuly

--- Centrum/Centrum/content.php
+++ Centrum/Centrum/content.php
@@ -4,10 +4,9 @@
 	<div id="content" class="well">
 	 
 		 <?php
-			if ( is_single() ) :
+			if (  !is_page() ) :
 				the_title( '<h1 class="entry-title">', '</h1>' );
-			else :
-				the_title( '<h2 class="entry-title">', '</h2>' );
+			
 				/* the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); */
 			endif;
 		?>
