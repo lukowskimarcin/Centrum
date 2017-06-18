482c6185c66c6a22df441ecf3a0cacb351d47d90
Marcin <Marcin@192.168.1.3>
2017-06-18 16:03:27 +0200

    menu test1

--- Centrum/template/js/centrum.js
+++ Centrum/template/js/centrum.js
@@ -1,9 +1,10 @@
 $(window).scroll(function() {
 		
-	if ($(document).scrollTop() > 50) {
+	if ($(document).scrollTop() > 30) {
 		$('nav').addClass('navbar-transparent-shrink');
 		$('.navbar-nav > li > a').css({'line-height' : '50px','height' : '50px'	});
-		$('.navbar-brand img').css("height", "50px");
+		$('.navbar-brand > a > img').css("height", "50px");
+		
 		$('.navbar-brand').addClass('navbar-height50');
 		$('.navbar-header').addClass('navbar-height50');
 		$('.navbar-toggle').addClass('navbar-toggle-shrink');
@@ -11,7 +12,8 @@ $(window).scroll(function() {
 	} else {
 		$('nav').removeClass('navbar-transparent-shrink');
 		$('.navbar-nav > li > a').css({'line-height' : '80px','height' : '80px'	});
-		$('.navbar-brand img').css("height", "80px");
+		$('.navbar-brand > a > img').css("height", "80px");
+		
 		$('.navbar-brand').removeClass('navbar-height50');
 		$('.navbar-header').removeClass('navbar-height50');
 		$('.navbar-toggle').removeClass('navbar-toggle-shrink');
@@ -21,6 +23,25 @@ $(window).scroll(function() {
 
 
 $(document).ready(function() {
+	//Menu dodanie 
+	$(".navbar-nav >  li").each(function(){
+    	//$(this).append('<div class="menu_hover"></div>');
+    });
+	
+	
+	 $(".navbar-nav >  li").mouseenter(function(){
+//	    	if ($(window).width() > 943) {
+	    		$(this).children(".menu_hover").css('display', 'block');
+	//    	}
+	    });
+	    
+	    $(".navbar-nav >  li").mouseleave(function(){
+	  //  	if ($(window).width() > 943) {
+	    		$(this).children(".menu_hover").css('display', 'none');
+	    //	}
+	   });
+	
+	
 	//Menu podswietlanie
 	$('.nav > li').click(function() {
 		$('.nav > li').removeClass('active');
@@ -28,7 +49,7 @@ $(document).ready(function() {
 	});
 	
 	$(".navbar-right > li > a").on('click', function(e) {
-		$(document).scrollTo($(this).attr('href'), 1000 );
+		$(document).scrollTo($(this).attr('href'), 1000, {offset: -50} );
 	});
 	
 });
