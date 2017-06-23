0146f071ed251f125049ac9a95f3ca77f2d63465
Marcin <Marcin@192.168.1.3>
2017-06-24 00:49:25 +0200

    menu

--- Centrum/template/js/centrum.js
+++ Centrum/template/js/centrum.js
@@ -4,23 +4,20 @@ $(window).scroll(function() {
 		$('nav').addClass('navbar-transparent-shrink');
 		$('.navbar-nav > li > a').css({'line-height' : '50px','height' : '50px'	});
 		$('.navbar-brand > a > img').css("height", "50px");
-		
 		$('.navbar-brand').addClass('navbar-height50');
 		$('.navbar-header').addClass('navbar-height50');
 		$('.navbar-toggle').addClass('navbar-toggle-shrink');
-		
 		$('.menu_hover').css("bottom", "10px");
 		
 	} else {
 		$('nav').removeClass('navbar-transparent-shrink');
 		$('.navbar-nav > li > a').css({'line-height' : '80px','height' : '80px'	});
 		$('.navbar-brand > a > img').css("height", "80px");
-		
 		$('.navbar-brand').removeClass('navbar-height50');
 		$('.navbar-header').removeClass('navbar-height50');
 		$('.navbar-toggle').removeClass('navbar-toggle-shrink');
-		
 		$('.menu_hover').css("bottom", "25px");
+		
 	}
 });
 
@@ -34,25 +31,44 @@ $(document).ready(function() {
 		}
 		i++;
     });
+
+ 
+	 $(".navbar-nav li").mouseenter(function() {
+		if($(this).hasClass("active")) {
+			$(this).removeClass('navbar-nav-hover');
+			$(this).children(".menu_hover").css('display', 'none');
+		} else {
+			$(this).addClass('navbar-nav-hover');
+			if ($(window).width() > 943) {
+				$(this).children(".menu_hover").css('display', 'block');
+			}
+		}
+	});
+	 
+
+	$(".navbar-nav li").not("active").mouseleave(function() {
+		if($(this).hasClass("active")) {
+			$(this).removeClass('navbar-nav-hover');
+			$(this).children(".menu_hover").css('display', 'none');
+		} else {
+			$(this).removeClass('navbar-nav-hover'); 
+			
+			if ($(window).width() > 943) {
+				$(this).children(".menu_hover").css('display', 'none');
+			}
+		}
+	});
+ 
 	
 	
-	 $(".navbar-nav li").not(".active").mouseenter(function(){
-//	    	if ($(window).width() > 943) {
-	 		$(this).children(".menu_hover").css('display', 'block');
-	//    	}
-	    });
-	    
-	    $(".navbar-nav li").not(".active").mouseleave(function(){
-	  //  	if ($(window).width() > 943) {
-	    	$(this).children(".menu_hover").css('display', 'none');
-	    //	}
-	   });
-	
-	
-	//Menu podswietlanie
+	// Menu podswietlanie
 	$('.nav > li').click(function() {
 		$('.nav > li').removeClass('active');
+		$('.nav > li').removeClass('navbar-nav-hover');
+		$('.nav > li').children(".menu_hover").css('display', 'none');
+		
 		$(this).addClass('active');
+		
 	});
 	
 	//Menu scrolowanie do pozycji
